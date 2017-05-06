0131ff1f5372a025edb76c5ccaade09668e85023
vinz243 <vinz243@gmail.com>
2017-05-06 18:54:27 +0200

    feat(albums-list-view): support filtering

--- client/src/features/library/components/AlbumListItem/AlbumListItem.tsx
+++ client/src/features/library/components/AlbumListItem/AlbumListItem.tsx
@@ -2,16 +2,30 @@ import * as React from 'react';
 import {LibraryActions} from '../../actions.d';
 import {Album, albumURI} from 'compactd-models';
 import {Link} from 'react-router-dom';
+import {MatchResult} from 'fuzzy';
 require('./AlbumListItem.scss');
 
 interface AlbumListItemProps {
   actions: LibraryActions;
   album: Album;
+  filterMatch?: MatchResult;
 }
 
 export class AlbumListItem extends React.Component<AlbumListItemProps, {}>{
   render (): JSX.Element {
-    const {actions, album} = this.props;
+    const {actions, album, filterMatch} = this.props;
+    let name: JSX.Element = <span className="not-filtered">{album.name}</span>;
+
+    if (filterMatch) {
+      const match = filterMatch.rendered.split('')
+        .map((char: string, i: number, arr: string[]) => {
+          if (char === '$') return <span className="empty"></span>;
+          if (arr[i - 1] === '$') return <span className="match">{char}</span>;
+          return <span className="not-match">{char}</span>
+        });
+      name = <span className="filtered">{match}</span>;
+    }
+
     return <div className="album-list-item">
       <Link to={`/library/${albumURI(album._id).name}`}>
         <div className="album-image">
@@ -19,7 +33,7 @@ export class AlbumListItem extends React.Component<AlbumListItemProps, {}>{
         </div>
         <div className="album-description">
           <span className="album-name">
-            {album.name}
+            {name}
           </span>
           <span className="album-track-count">15 albums</span>
         </div>
--- client/src/features/library/components/AlbumsListView/AlbumsListView.tsx
+++ client/src/features/library/components/AlbumsListView/AlbumsListView.tsx
@@ -3,16 +3,28 @@ import {LibraryActions} from '../../actions.d';
 import {LibraryState} from 'definitions';
 import {AlbumListItem} from '../AlbumListItem';
 import ScrollableDiv from 'components/ScrollableDiv';
+import {Album} from 'compactd-models';
+import * as fuzzy from 'fuzzy';
 
 require('./AlbumsListView.scss');
 
 interface AlbumsListViewProps {
   actions: LibraryActions;
   artist: string;
-  library: LibraryState
+  library: LibraryState;
 }
 
-export class AlbumsListView extends React.Component<AlbumsListViewProps, {}>{
+export class AlbumsListView extends React.Component<AlbumsListViewProps, {
+  albumsFilter: string;
+}>{
+  constructor () {
+    super();
+    this.state = {albumsFilter: ''};
+  }
+  handleAlbumsFilterChange (evt: Event) {
+    const target = evt.target as HTMLInputElement;
+    this.setState({albumsFilter: target.value});
+  }
   componentWillReceiveProps (nextProps: AlbumsListViewProps) {
     if (nextProps.artist !== this.props.artist) {
       if (!nextProps.artist) return this.props.actions.fetchAllAlbums();
@@ -26,11 +38,34 @@ export class AlbumsListView extends React.Component<AlbumsListViewProps, {}>{
   render (): JSX.Element {
     const {actions, library} = this.props;
     const artist = this.props.artist ?
-      library.artistsById[`library/${this.props.artist}`] : {name: '', albums: library.albums};
+      (library.artistsById[`library/${this.props.artist}`]
+        || {name: '', albums: []}) : {name: '', albums: library.albums};
 
-    const albums = artist ? artist.albums.map((album) => {
-      return <AlbumListItem album={album} actions={actions} />
-    }) : [];
+    const options = {
+      pre: '$', post: '',
+      extract: (el: Album) => el.name
+    };
+    const albums = artist.albums
+    .map(album => {
+      if (this.state.albumsFilter) {
+        return [
+          fuzzy.match(this.state.albumsFilter, album.name, options), album
+        ]
+      }
+      return [undefined, album];
+    }).filter(([matched, album]: [fuzzy.MatchResult, Album]) => {
+      return this.state.albumsFilter ? matched : true;
+    }).sort((a, b) => {
+      if (!this.state.albumsFilter) {
+        if ((a[1] as Album).name > (b[1] as Album).name) return 1;
+        if ((a[1] as Album).name < (b[1] as Album).name) return -1;
+        return 0;
+      }
+      return (b[0] as fuzzy.MatchResult).score - (a[0] as fuzzy.MatchResult).score;
+    }).map(([matched, album]: [fuzzy.MatchResult, Album]) => {
+      return  <AlbumListItem key={album._id} filterMatch={matched}
+                album={album} actions={actions} />
+    })
 
     const header = this.props.artist ?
     <div className="artist-header">
@@ -41,6 +76,8 @@ export class AlbumsListView extends React.Component<AlbumsListViewProps, {}>{
     </div>  : <div className="pt-input-group">
           <span className="pt-icon pt-icon-search"></span>
           <input className="pt-input" type="search"
+            onChange={this.handleAlbumsFilterChange.bind(this)}
+            value={this.state.albumsFilter}
             placeholder="Filter albums" dir="auto" />
         </div>
 
