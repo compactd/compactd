8dd73962c27397fcc768fe4106eb59de07891d66
vinz243 <vinz243@gmail.com>
2017-05-06 18:35:22 +0200

    feat(albums-list-view): header indicates artist

--- client/src/features/library/components/AlbumsListView/AlbumsListView.tsx
+++ client/src/features/library/components/AlbumsListView/AlbumsListView.tsx
@@ -20,24 +20,33 @@ export class AlbumsListView extends React.Component<AlbumsListViewProps, {}>{
     }
   }
   componentDidMount () {
-    console.log(this.props);
     if (!this.props.artist) return this.props.actions.fetchAllAlbums();
     this.props.actions.fetchArtist(this.props.artist);
   }
   render (): JSX.Element {
     const {actions, library} = this.props;
     const artist = this.props.artist ?
-      library.artistsById[`library/${this.props.artist}`] : {albums: library.albums};
+      library.artistsById[`library/${this.props.artist}`] : {name: '', albums: library.albums};
+
     const albums = artist ? artist.albums.map((album) => {
       return <AlbumListItem album={album} actions={actions} />
     }) : [];
-    return <div className="albums-list-view">
-      <div className="list-header">
-        <div className="pt-input-group">
+
+    const header = this.props.artist ?
+    <div className="artist-header">
+      <div className="artist-image">
+        <img src="http://placehold.it/32x32" />
+      </div>
+      <div className="artist-name">Albums by {(artist || {name: ''}).name}</div>
+    </div>  : <div className="pt-input-group">
           <span className="pt-icon pt-icon-search"></span>
           <input className="pt-input" type="search"
-            placeholder="Filter artists" dir="auto" />
+            placeholder="Filter albums" dir="auto" />
         </div>
+
+    return <div className="albums-list-view">
+      <div className="list-header">
+        {header}
       </div>
       <ScrollableDiv>
         <div className="albums-container">
