91f1aaef550321b5b0f60c9a4b4c9980e3e72e6a
vinz243 <vinz243@gmail.com>
2017-10-29 14:05:52 +0100

    feat(app/database): sync db api

--- client/src/app/database.ts
+++ client/src/app/database.ts
@@ -10,3 +10,20 @@ export const getDatabase = function<T> (name: string) {
     }
   });
 }
+
+
+function syncDB(local: PouchDB.Database, remote: PouchDB.Database) {
+  return new Promise((resolve, reject) => {
+    local.sync(remote).on('complete', () => {
+      resolve();
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+export const syncDatabases = function (...dbs: string[]) {
+  return Promise.all(dbs.map((db) => {
+    return syncDB(new PouchDB(db), getDatabase(db));
+  }));
+}
