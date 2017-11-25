efc0a5784a318b634837a2c128ce31e39b4ac70d
vinz243 <vinz243@gmail.com>
2017-11-25 13:43:37 +0100

    fix(configure): increase logging and remove validators and config docs #34

--- server/src/features/configure/DatabaseConfigurator.ts
+++ server/src/features/configure/DatabaseConfigurator.ts
@@ -10,6 +10,7 @@ import * as Analytics from '../analytics/Analytics';
 import {createViews} from '../utils/library-utils';
 import * as path from 'path';
 import config from '../../config';
+import {mainStory} from 'storyboard';
 
 export interface ISDAOptions {
   adminPassword: string;
@@ -30,84 +31,89 @@ export class DatabaseConfigurator {
     // await this.startServer();
     await this.endAdminParty();
     await this.configureDatabases();
-    await this.createConfig();
     await this.setupNode();
     await Analytics.initialize();
     await createViews();
     return;
   }
 
-  createConfig (): Promise<any> {
-    const db = new PouchDB('config');
-
-    return Promise.all(baseConfig.map((({key, value}) => {
-      return db.put({
-        _id: key, value
-      } as any);
-    })));
-  }
-
   configureDatabases (): Promise<any>{
-    return Promise.all(dbs.map(({name, schema, perms}) => {
-      const db = new PouchDB(name);
-
-      return db.put({
-        _id: `_design/validator`,
-        validate_doc_update: createValidator(schema, perms)
-      } as any);
-    })).then(() => {});
+    // return Promise.all(dbs.map(({name, schema, perms}) => {
+    //   const db = new PouchDB(name);
+    //   mainStory.info('configure', )
+    //   return db.put({
+    //     _id: `_design/validator`,
+    //     validate_doc_update: createValidator(schema, perms)
+    //   } as any);
+    // })).then(() => {});
+    return Promise.resolve({});
   }
 
   async setupNode () {
-    const res: any = await fetch(`http://${
-        this.opts.couchHost
-      }:${this.opts.couchPort}/_cluster_setup`, {
+    mainStory.info('configure', `Trying to setup node...`);
+
+    const url = `http://${
+      this.opts.couchHost
+    }:${this.opts.couchPort}/_cluster_setup`;
+
+    const body = {
+      action: "enable_single_node",
+      username: this.opts.adminUsername,
+      password: this.opts.adminPassword,
+      bind_address: "0.0.0.0",
+      singlenode: true,
+      port: config.get('couchPort')
+    };
+
+    mainStory.info('configure', `POST ${url}`, {
+      attach: {...body, password: '<written out>'},
+      attachLevel: 'debug'
+    });
+
+    const res: any = await fetch(url, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           Authorization: 'Basic ' + new Buffer(
             this.opts.adminUsername + ':' + this.opts.adminPassword).toString('base64')
         },
-        body: JSON.stringify({
-          action: "enable_cluster",
-          username: this.opts.adminUsername,
-          password: this.opts.adminPassword,
-          bind_address: "0.0.0.0",
-          port: config.get('couchPort')})
+        body: JSON.stringify(body)
       }).then((res) => res.json());
 
-    const fres: any = await fetch(`http://${
-        this.opts.couchHost
-      }:${this.opts.couchPort}/_cluster_setup`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Basic ' + new Buffer(
-          this.opts.adminUsername + ':' + this.opts.adminPassword).toString('base64')
-      },
-      body: JSON.stringify({
-        action: "finish_cluster"
-      })
-    }).then((res) => res.json());
+    mainStory.info('configure', 'CouchDB responded with', {attach: res});
   }
 
   async endAdminParty () {
     const node = await this.getNode();
-    const res: any = await fetch(`http://${
-        this.opts.couchHost
-      }:${this.opts.couchPort}/_node/${
-        node}/_config/admins/${this.opts.adminUsername}`, {
+
+    mainStory.info('configure', 'Ending admin party...');
+
+    const url = `http://${
+      this.opts.couchHost
+    }:${this.opts.couchPort}/_node/${
+      node
+    }/_config/admins/${this.opts.adminUsername}`;
+
+    mainStory.info('configure', 'PUT ' + url);
+
+    const res: any = await fetch(url, {
         method: 'PUT',
         body: `"${this.opts.adminPassword}"`
       }).then((res) => res.json());
+
+    mainStory.info('configure', 'CouchDB responded with', {attach: res});
   }
   getNode (): Promise<string> {
-    return fetch(`http://${
+    mainStory.info('configure', 'Get node name...');
+    const url = `http://${
       this.opts.couchHost
-    }:${this.opts.couchPort}/_membership`)
+    }:${this.opts.couchPort}/_membership`
+    mainStory.info('configure', 'GET '+ url)
+    return fetch(url)
     .then((response: Response) => {
       return response.json();
     }).then((res: any) => {
+      mainStory.info('configure', 'CouchDB responded with', {attach: res});
       assert.equal(res.all_nodes.length, 1);
       return res.all_nodes[0];
     });
