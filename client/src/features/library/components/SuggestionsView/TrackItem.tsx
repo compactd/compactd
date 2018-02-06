0e90dda13a8f4d1df0183d8ace8f76ae6575f156
vinz243 <vinz243@gmail.com>
2018-02-06 14:07:04 +0100

    fix(library): don't use topTracks to start playback fixes #44

--- client/src/features/library/components/SuggestionsView/TrackItem.tsx
+++ client/src/features/library/components/SuggestionsView/TrackItem.tsx
@@ -30,8 +30,7 @@ export default class TrackItem extends React.Component<TrackItemProps, {}> {
       this.handleAltClick(evt);
       return;
     }
-    const stack = library.topTracks.map((el) => el.key).slice(this.props.index);
-    actions.replacePlayerStack(stack);
+    actions.replacePlayerStack([this.props.track]);
   }
   private handleAltClick (evt: MouseEvent) {
     evt.stopPropagation();
