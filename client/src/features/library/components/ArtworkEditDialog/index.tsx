3efc4c00e93aa043a7897557b74a6c80612da2c5
vinz243 <vinz243@gmail.com>
2018-02-08 21:08:27 +0100

    style(library): better album artwork edit dialog

--- client/src/features/library/components/ArtworkEditDialog/index.tsx
+++ client/src/features/library/components/ArtworkEditDialog/index.tsx
@@ -12,17 +12,21 @@ interface ArtworkEditDialogProps {
   onClose: (event?: React.SyntheticEvent<HTMLElement>) => void;
 }
 
+const BLANK_IMAGE = 'data:image/png;base64,R0lGODlhFAAUAIAAAP///wAAACH5BAEAAAAALAAAAAAUABQAAAIRhI+py+0Po5y02ouz3rz7rxUAOw==';
+
 export default class ArtworkEditDialog extends React.Component<ArtworkEditDialogProps, {
   data: {
     results: {
       url: string,
       dimensions: [number, number]
     }[]
-  }
+  },
+  itemHeight: number
 }> {
+  resultsDiv: HTMLDivElement;
   constructor () {
     super();
-    this.state = {data: {results: []}};
+    this.state = {data: {results: new Array(12)}, itemHeight: 64};
   }
   componentWillReceiveProps(nextProps: ArtworkEditDialogProps) {
     if (nextProps.isOpen && !this.props.isOpen) {
@@ -32,10 +36,18 @@ export default class ArtworkEditDialog extends React.Component<ArtworkEditDialog
       });
     }
   }
+  componentDidUpdate () {
+    const firstChild = this.resultsDiv.children[0];
+    const {width} = this.resultsDiv.getBoundingClientRect();
+    const calculatedWidth = (width / 5) - (5 * 5);
+    if (this.state.itemHeight !== calculatedWidth) {
+      this.setState({itemHeight: calculatedWidth});
+    }
+  }
   render () {
     const {item, isOpen, onClose} = this.props;
-    const results = this.state.data.results.slice(0, 10).map((res) => {
-      return <div className="image-container" key={res.url} onClick={() => {
+    const results = this.state.data.results.slice(0, 10).map((res, index) => {
+      return <div className="image-container" key={index} onClick={() => {
         onClose();
         Session.fetch(`/api/aquarelle/${item}/remote`, {
           method: 'PUT',
@@ -45,8 +57,16 @@ export default class ArtworkEditDialog extends React.Component<ArtworkEditDialog
           headers: {'Content-Type': 'application/json'}
         });
        
-      }}>
-        <BetterImage src={res.url} height={128} />
+      }} style={{
+        height: this.state.itemHeight + 'px',
+        width: this.state.itemHeight + 'px',
+        background: `url('${res.url || BLANK_IMAGE}') no-repeat center center scroll`,
+        backgroundSize: '100% 100%'
+      } as any}>
+        {/* <div
+          height={this.state.itemHeight + 'px'}
+          width={this.state.itemHeight + 'px'}
+          style={{display: 'block'}} /> */}
         <div className="image-info">
           {res.dimensions[0]}x{res.dimensions[1]}
         </div>
@@ -55,7 +75,7 @@ export default class ArtworkEditDialog extends React.Component<ArtworkEditDialog
     return <Dialog isOpen={isOpen} onClose={onClose} title="Change artwork">
       <div className="result-row">
         <div className="header">Google image results</div>
-        <div className="content">{results}</div>
+        <div className="content" ref={(ref) => this.resultsDiv = ref}>{results}</div>
       </div>
       <div className="result-row">
         <div className="header">Upload Custom Image</div>
